a7d4ed0687dc3a5c13a65286169a4054cc10aeba
vizn3r <viznertw@gmail.com>
2024-01-01 18:37:09 +0100

    WIP: WebSockets

--- com/ws.go
+++ com/ws.go
@@ -1,49 +1,80 @@
 package com
 
 import (
+	"flag"
 	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
 
-	"github.com/gofiber/contrib/websocket"
-	"github.com/gofiber/fiber/v2"
+	"github.com/fasthttp/websocket"
 )
 
+var addr = flag.CommandLine.String("addr", "localhost:3000", "http service address")
+
 func ConnectWS() {
-	app := fiber.New()
+	flag.Parse()
+	log.SetFlags(0)
 
-	app.Use("/ws", func (c *fiber.Ctx) error  {
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{
+		Scheme: "ws",
+		Host:   *addr,
+		Path:   "/ws",
+	}
+	log.Println("Connecting to:", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("Dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("Write:", err)
+				return
+			}
+			log.Println("Recv:", msg)
 		}
-		return fiber.ErrUpgradeRequired
-	})
+	}()
 
-	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
-		log.Println(c.Locals("allowed"))  
-        log.Println(c.Params("id"))       
-        log.Println(c.Query("v"))         
-        log.Println(c.Cookies("session"))
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 
-		var (
-            mt  int
-            msg []byte
-            err error
-        )
-        for {
-            if mt, msg, err = c.ReadMessage(); err != nil {
-                log.Println("read:", err)
-                break
-            }
-            log.Printf("recv: %s %s", msg, c.Params("asd", "def"))
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
 
-			
-
-            if err = c.WriteMessage(mt, msg); err != nil {
-                log.Println("write:", err)
-                break
-            }
-        }	
-	}))
-	
-	log.Fatal(app.Listen(":3000"))
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
